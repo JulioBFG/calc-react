5dc345d26faf9103fc0a6636e4fb3591ba48a83b
Julio Lopes <juliolopes.bfg@gmail.com>
2019-09-15 23:30:29 -0300

    Add: projeto total

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/calculator.jsx';
+
+ReactDOM.render(
+    <div>
+        <h1>Calculadora Do Lopes :)</h1>
+        <Calculator />
+    </div>
+, document.getElementById('root'));
+
